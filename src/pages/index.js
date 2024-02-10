2100fb115b18fdc35f18a1ddd65168c7af424558
azyAngela <114364515+azyAngela@users.noreply.github.com>
2024-02-10 10:03:09 -0500

    quiz

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,23 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
+import Profile from './qcomps/profile_mistake'
+import MyComp from './qcomps/firstcomp'
+import Bios from './qcomps/bios'
+import TodoList from './qcomps/todos'
+import ProfilePros from './components/profile_props'
+import PackingList from './components/props_item'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        {/* <Profile /> */}
+        {/* <MyComp /> */}
+        {/* <Bios /> */}
+        {/* <TodoList /> */}
+        {/* <ProfilePros /> */}
+        {/* <Gallery /> */}
+        <PackingList />
     </div>
   )
 }
--- src/pages/qcomps/firstcomp.js
+++ src/pages/qcomps/firstcomp.js
@@ -1,1 +1,11 @@
-function MyComp() {}
+
+export default function MyComp() {
+    return(<div>
+        <h1>My first component</h1>
+        <o1>
+            <li>Components: UI Building Block</li>
+            <li>Defining a Component</li>
+            <li>Using a component</li>
+        </o1>
+    </div>)
+}
